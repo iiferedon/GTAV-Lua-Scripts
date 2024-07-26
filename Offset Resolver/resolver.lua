6cb7d86369252fa7bb34f3af73092121febc1ede
iiferedon <97226864+iiferedon@users.noreply.github.com>
2024-07-26 20:08:00 +1000

    Update resolver.lua

--- Offset Resolver/resolver.lua
+++ Offset Resolver/resolver.lua
@@ -1,4 +1,7 @@
 --roulette tables
+local natives = FileMgr.GetMenuRootPath() .. "\\Lua\\natives.lua"
+dofile(natives)
+
 local baseaddress = Memory.GetBaseAddress("GTA5.exe")
 
 local function resolver(baseaddress, offsets, write_console)
@@ -17,12 +20,25 @@ local function resolver(baseaddress, offsets, write_console)
     return addr
 end
 
-local handle = baseaddress + 0x0296AD10
-
-local offset_table = {
-{0, 0xF28, 0x108, 0x4F8},
+local read_tables = {
+    {0, 0xF28, 0x108, 0x4F8},
     {0, 0xF28, 0x108, 0x500},
     {0, 0xF28, 0x108, 0x530},
     {0, 0xF28, 0x108, 0x538}
 }
-resolver(handle,offset_table, true)
+
+local write_table = 
+{0, 0xF28, 0x108
+}
+
+local roulettehandle = baseaddress + 0x0296AD10
+
+for _, read_table in ipairs(read_tables) do
+    resolver(roulettehandle, read_table, true)
+end
+
+
+Memory.WriteUInt(resolver(roulettehandle,write_table) + 0x4F8, 8)
+Memory.WriteUInt(resolver(roulettehandle,write_table) + 0x500, 8)
+Memory.WriteUInt(resolver(roulettehandle,write_table) + 0x530, 8)
+Memory.WriteUInt(resolver(roulettehandle,write_table) + 0x538, 8)
