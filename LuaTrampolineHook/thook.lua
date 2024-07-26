e2c454366a506ff74aa652a3add8e81888ce0c73
iiferedon <97226864+iiferedon@users.noreply.github.com>
2024-07-26 20:13:49 +1000

    Update thook.lua

--- LuaTrampolineHook/thook.lua
+++ LuaTrampolineHook/thook.lua
@@ -1,7 +1,7 @@
-
 local baseaddress = Memory.GetBaseAddress("GTA5.exe")
-local targetFuncAddr = baseaddress + 0x183A640 
-
+local pattern = "48 8D 0D ? ? ? ? F3 0F 10 04 81 F3 0F 11 05"
+local address = Memory.Scan(pattern, "GTA5.exe")
+local targetFuncAddr = address
 local trampolineSize = 12
 
 local trampoline = Memory.Alloc(trampolineSize + 5)
@@ -16,13 +16,13 @@ for i = 0, trampolineSize - 1 do
 end
 
 local returnAddress = targetFuncAddr + trampolineSize
-local jumpBack = { 0xE9, (returnAddress - trampoline - trampolineSize - 5) & 0xFF, ((returnAddress - trampoline - trampolineSize - 5) >> 8) & 0xFF, ((returnAddress - trampoline - trampolineSize - 5) >> 16) & 0xFF, ((returnAddress - trampoline - trampolineSize - 5) >> 24) & 0xFF }
+local jumpBack = {0xE9, (returnAddress - trampoline - trampolineSize - 5) & 0xFF, ((returnAddress - trampoline - trampolineSize - 5) >> 8) & 0xFF, ((returnAddress - trampoline - trampolineSize - 5) >> 16) & 0xFF, ((returnAddress - trampoline - trampolineSize - 5) >> 24) & 0xFF}
 for i = 0, 4 do
     Memory.WriteByte(trampoline + trampolineSize + i, jumpBack[i])
 end
 
 local hookFuncAddr = Memory.Alloc(5)
-local hook = { 0xE9, (hookFuncAddr - targetFuncAddr - 5) & 0xFF, ((hookFuncAddr - targetFuncAddr - 5) >> 8) & 0xFF, ((hookFuncAddr - targetFuncAddr - 5) >> 16) & 0xFF, ((hookFuncAddr - targetFuncAddr - 5) >> 24) & 0xFF }
+local hook = {0xE9, (hookFuncAddr - targetFuncAddr - 5) & 0xFF, ((hookFuncAddr - targetFuncAddr - 5) >> 8) & 0xFF, ((hookFuncAddr - targetFuncAddr - 5) >> 16) & 0xFF, ((hookFuncAddr - targetFuncAddr - 5) >> 24) & 0xFF}
 
 for i = 0, 4 do
     Memory.WriteByte(targetFuncAddr + i, hook[i])
@@ -30,7 +30,6 @@ end
 
 function hookFunction()
     print("Hooked function called!")
-    
     Memory.LuaCallCFunction(trampoline)
 end
 
@@ -48,9 +47,7 @@ function targetFunction()
     print("Original function called!")
 end
 
-local Address = Memory.Scan
+local valueAtAddress = Memory.ReadByte(address)
+Logger.Log(eLogColor.RED, "PATTERN SCAN", ("Value at 0x%X equals 0x%X"):format(address, valueAtAddress))
 
-local Address = Memory.Scan("48 8D 0D ? ? ? ? F3 0F 10 04 81 F3 0F 11 05", "GTA5.exe")
-local valueAtAddress = Memory.ReadByte(Address)
-Logger.Log(eLogColor.RED, "PATTERN SCAN", ("Value at 0x%X quals 0x%X"):format(Address, valueAtAddress))
 targetFunction()
