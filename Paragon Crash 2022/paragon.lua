c1f7470a54be894374980140df0cf3bccf0c54aa
iiferedon <97226864+iiferedon@users.noreply.github.com>
2025-01-31 22:07:14 +1100

    Create paragon.lua

---
+++ Paragon Crash 2022/paragon.lua
@@ -0,0 +1,8 @@
+function paragon_kick(pid)
+        g_util.trigger_script_event(pid, {1347850743, pid, 532165, pid})
+        g_util.trigger_script_event(pid, {-1212832151, pid, 532165, pid})
+        g_util.trigger_script_event(pid, {1428412924, pid, 532165, pid})
+        g_util.trigger_script_event(pid, {-435067392, pid, 532165, pid})
+        g_util.trigger_script_event(pid, {-799924696, pid, 532165, pid})
+        g_util.trigger_script_event(pid, {1428412924, pid, 532165, pid})   
+    end 
